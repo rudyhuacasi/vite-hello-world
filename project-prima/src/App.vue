f636dd2836a8e16e7cfad89e5a8406c448700966
rudyhuacasi <alexisitaliano16@gmail.com>
2024-06-04 17:19:40 +0200

    {UPD} added scaffolding projec-prima (vite)

---
+++ project-prima/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import header from "";
+  export default {
+    name: `corpo`,
+    components:{
+
+    }
+  }
+</script>
+<template>
+
+</template>
+<style>
+
+</style>
---
+++ project-prima/src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    name: app,
+    data() {
+      return{
+        title:  "La mia prima app con Vite!"
+      };
+    },
+  }
+</script>
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+
+</template>
+<style>
+
+</style>
